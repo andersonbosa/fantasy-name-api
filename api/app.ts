2e8b8d0d11ba9137305a7df2c026642604dafcc5
andersonbosa <andersonbosa0@gmail.com>
2024-04-23 10:00:18 -0300

    feat: implemented error handling and updated documentation
    - Added a custom error handler to provide a better user experience in case of errors.
    - Updated the documentation to reflect the updated API route for generating names.

--- api/app.ts
+++ api/app.ts
@@ -2,6 +2,10 @@ import { Hono } from 'hono'
 import { etag } from 'hono/etag'
 import { logger } from 'hono/logger'
 
+/**
+ * @hono_setup
+ */
+
 const app = new Hono({}).basePath('/api')
 
 app.use(logger())
@@ -12,24 +16,33 @@ app.notFound((c) => {
   return c.redirect('/api')
 })
 
+app.onError((err, c) => {
+  console.error(`${err}`)
+  return c.text('Custom Error Message', 500)
+})
+
+/**
+ * @hono_routes
+ */
+
 app.get('/', (c) => {
   return c.text(`
 Hello, friend. 👾
 
 This project aims to provide an API using the beautiful fantasy name generator developed by "Edgar Alvarado (Pe1uca)". My thanks to him.
 
-I just encapsuated to a framework to make available via API. Check it out \`${c.req.url}/docs\`  for more details. 
+I just encapsuated to a framework to make available via API. Check it out \`${c.req.url}/docs\`  for more details.
 
 Have fun!
   `)
 })
 
-
 app.get('/docs', (c) => {
   return c.text(`TODO`)
 })
 
-app.get('/:pattern', (c) => {
+app.get('/generate/:pattern', (c) => {
+
   return c.json({
     data: [],
     metadata: {},
