0292aa961bf2968299dd4870a484b8640ad5cea6
andersonbosa <andersonbosa0@gmail.com>
2024-05-16 21:25:03 -0300

    docs: add documentation and home response

--- api/app.ts
+++ api/app.ts
@@ -5,6 +5,7 @@ import { secureHeaders } from 'hono/secure-headers'
 import { cors } from 'hono/cors'
 import { prettyJSON } from 'hono/pretty-json'
 import { NameGenController } from './namegen.controller'
+import { docsResponse, homeResponse } from './constants'
 
 /**
  * @hono_setup
@@ -40,69 +41,18 @@ app.onError((err, c) => {
 })
 
 app.get('/', (c: Context) => {
-  return c.text(`
-Hello, friend. 👾
-
-This project aims to provide an API using the beautiful fantasy name generator developed by "Edgar Alvarado (Pe1uca)". My thanks to him.
-
-I just encapsuated to a framework to make available via API. Check it out \`${c.req.url}/docs\`  for more details.
-
-Have fun!
-  `)
+  const originUrl = c.req.url
+  return c.text(homeResponse(originUrl))
 })
 
 app.get('/docs', (c: Context) => {
   let snippet = c.req.query('snippet') ?? ''
+  const originUrl = c.req.url
 
   const currentOriginUrl = new URL(c.req.url.replace('/api/docs', ''))
   const currentOrigin = currentOriginUrl.origin.toString()
 
-  return c.text(`
-# Documentation
-
-> hack: use snippet param on url to change help with your tests :)
-> example: ${c.req.url}?snippet=wget
-
-# Syntax
-
-${snippet} ${currentOrigin}/api/generate/:pattern
-${snippet} ${currentOrigin}/api/generate/:pattern?limit=1000
-${snippet} ${currentOrigin}/api/generate/:pattern?limit=1000&pretty
-
-
-# Usage examples
-
-${snippet} ${currentOrigin}/api/generate/justAstrAsPattern
-${snippet} ${currentOrigin}/api/generate/MIDDLE_EARTH?limit=1000&pretty
-${snippet} ${currentOrigin}/api/generate/pOkEmOn
-${snippet} ${currentOrigin}/api/generate/CHINESE_NAMES
-${snippet} ${currentOrigin}/api/generate/FANTASY_S_E
-
-
-# Existing patterns
-
-> its usage is case-insesitive.
-
-- MIDDLE_EARTH
-- JAPANESE_NAMES_CONSTRAINED
-- JAPANESE_NAMES_DIVERSE
-- CHINESE_NAMES
-- GREEK_NAMES
-- HAWAIIAN_NAMES_1
-- HAWAIIAN_NAMES_2
-- OLD_LATIN_PLACE_NAMES
-- DRAGONS_PERN
-- DRAGON_RIDERS
-- POKEMON
-- FANTASY_VOWELS_R
-- FANTASY_S_A
-- FANTASY_H_L
-- FANTASY_N_L
-- FANTASY_K_N
-- FANTASY_J_G_Z
-- FANTASY_K_J_Y
-- FANTASY_S_E
-`)
+  return c.text(docsResponse(originUrl, snippet, currentOrigin))
 })
 
 app.get('/generate/:pattern', NameGenController)
---
+++ api/constants.ts
@@ -0,0 +1,63 @@
+
+export const homeResponse = (originUrl: string): string => {
+  return `
+Hello, friend! 👾
+
+This project aims to provide an API using the beautiful fantasy name generator
+developed by "Edgar Alvarado (Pe1uca)". My thanks to him.
+
+I just encapsuated to a framework to make available via API.
+Check it out \`${originUrl}/docs\` for more details.
+
+Have fun!`
+}
+
+
+export const docsResponse = (originUrl: string, snippet: string, currentOrigin: string,): string => {
+  return `
+# Documentation
+
+> hack: use 'snippet' param on url to improve your experience :)
+> example: ${originUrl}?snippet=curl
+
+
+# Syntax
+
+${snippet} ${currentOrigin}/api/generate/:pattern
+${snippet} ${currentOrigin}/api/generate/:pattern?limit=1000
+${snippet} ${currentOrigin}/api/generate/:pattern?limit=1000&pretty
+
+
+# Usage examples
+
+${snippet} ${currentOrigin}/api/generate/justAstrAsPattern
+${snippet} ${currentOrigin}/api/generate/MIDDLE_EARTH?limit=1000&pretty
+${snippet} ${currentOrigin}/api/generate/pOkEmOn
+${snippet} ${currentOrigin}/api/generate/CHINESE_NAMES
+${snippet} ${currentOrigin}/api/generate/FANTASY_S_E
+
+
+# Existing patterns
+
+> its usage is case-insesitive.
+
+- MIDDLE_EARTH
+- JAPANESE_NAMES_CONSTRAINED
+- JAPANESE_NAMES_DIVERSE
+- CHINESE_NAMES
+- GREEK_NAMES
+- HAWAIIAN_NAMES_1
+- HAWAIIAN_NAMES_2
+- OLD_LATIN_PLACE_NAMES
+- DRAGONS_PERN
+- DRAGON_RIDERS
+- POKEMON
+- FANTASY_VOWELS_R
+- FANTASY_S_A
+- FANTASY_H_L
+- FANTASY_N_L
+- FANTASY_K_N
+- FANTASY_J_G_Z
+- FANTASY_K_J_Y
+- FANTASY_S_E`
+}
